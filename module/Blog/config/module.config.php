b900f9f21fa4b14989a6205f67681c6c3b16053a
jacobe911 <axmuzza@gmail.com>
2017-10-11 22:49:38 +0100

    complete zend tutorial

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -8,18 +8,22 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 return [
     'service_manager' => [
         'aliases' => [
-            // Update this line:
             Model\PostRepositoryInterface::class => Model\ZendDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\PostCommand::class,
+            Model\PostCommandInterface::class => Model\ZendDbSqlCommand::class,
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
-            // Add this line:
             Model\ZendDbSqlRepository::class => Factory\ZendDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\ZendDbSqlCommand::class => Factory\ZendDbSqlCommandFactory::class,
         ],
     ],
 	'controllers' => [
         'factories' => [
             Controller\ListController::class => Factory\ListControllerFactory::class,
+            Controller\WriteController::class => Factory\WriteControllerFactory::class,
+            Controller\DeleteController::class => Factory\DeleteControllerFactory::class,
         ],
     ],
     // This lines opens the configuration for the RouteManager
@@ -42,6 +46,42 @@ return [
                             'route'    => '/:id',
                             'defaults' => [
                                 'action' => 'detail',
+                            ],
+                            'constraints' => [
+                                'id' => '[1-9]\d*',
+                            ],
+                        ],
+                    ],
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route'    => '/add',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'add',
+                            ],
+                        ],
+                    ],
+                    'edit' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route'    => '/edit/:id',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'edit',
+                            ],
+                            'constraints' => [
+                                'id' => '[1-9]\d*',
+                            ],
+                        ],
+                    ],
+                    'delete' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route' => '/delete/:id',
+                            'defaults' => [
+                                'controller' => Controller\DeleteController::class,
+                                'action'     => 'delete',
                             ],
                             'constraints' => [
                                 'id' => '[1-9]\d*',
